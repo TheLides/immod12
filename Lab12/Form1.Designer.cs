a617e2d3b795de3b261c9211b2405dc53ddf5fcb
Rina Trofimova <lidesrose@gmail.com>
2021-05-08 23:16:25 +0700

    Done

---
+++ Lab12/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿
+namespace Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lambda1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lambda2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lambda3 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lambda4 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lambda5 = new System.Windows.Forms.NumericUpDown();
+            this.lambda6 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lambda7 = new System.Windows.Forms.NumericUpDown();
+            this.lambda8 = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.winnerLabel = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultLabel2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lambda1
+            // 
+            this.lambda1.Location = new System.Drawing.Point(80, 35);
+            this.lambda1.Name = "lambda1";
+            this.lambda1.Size = new System.Drawing.Size(120, 22);
+            this.lambda1.TabIndex = 0;
+            this.lambda1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Team 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lambda:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Team 2";
+            // 
+            // lambda2
+            // 
+            this.lambda2.Location = new System.Drawing.Point(268, 37);
+            this.lambda2.Name = "lambda2";
+            this.lambda2.Size = new System.Drawing.Size(120, 22);
+            this.lambda2.TabIndex = 4;
+            this.lambda2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Team 3";
+            // 
+            // lambda3
+            // 
+            this.lambda3.Location = new System.Drawing.Point(456, 37);
+            this.lambda3.Name = "lambda3";
+            this.lambda3.Size = new System.Drawing.Size(120, 22);
+            this.lambda3.TabIndex = 6;
+            this.lambda3.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(582, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Team 4";
+            // 
+            // lambda4
+            // 
+            this.lambda4.Location = new System.Drawing.Point(644, 37);
+            this.lambda4.Name = "lambda4";
+            this.lambda4.Size = new System.Drawing.Size(120, 22);
+            this.lambda4.TabIndex = 8;
+            this.lambda4.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Team 5";
+            // 
+            // lambda5
+            // 
+            this.lambda5.Location = new System.Drawing.Point(80, 83);
+            this.lambda5.Name = "lambda5";
+            this.lambda5.Size = new System.Drawing.Size(120, 22);
+            this.lambda5.TabIndex = 10;
+            this.lambda5.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // lambda6
+            // 
+            this.lambda6.Location = new System.Drawing.Point(268, 85);
+            this.lambda6.Name = "lambda6";
+            this.lambda6.Size = new System.Drawing.Size(120, 22);
+            this.lambda6.TabIndex = 11;
+            this.lambda6.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(206, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Team 6";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(394, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Team 7";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(582, 85);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 17);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Team 8";
+            // 
+            // lambda7
+            // 
+            this.lambda7.Location = new System.Drawing.Point(456, 85);
+            this.lambda7.Name = "lambda7";
+            this.lambda7.Size = new System.Drawing.Size(120, 22);
+            this.lambda7.TabIndex = 15;
+            this.lambda7.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lambda8
+            // 
+            this.lambda8.Location = new System.Drawing.Point(644, 85);
+            this.lambda8.Name = "lambda8";
+            this.lambda8.Size = new System.Drawing.Size(120, 22);
+            this.lambda8.TabIndex = 16;
+            this.lambda8.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(838, 138);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 17);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Winner:";
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.AutoSize = true;
+            this.winnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.winnerLabel.Location = new System.Drawing.Point(858, 166);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(44, 17);
+            this.winnerLabel.TabIndex = 18;
+            this.winnerLabel.Text = "none";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(11, 138);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(59, 17);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Results:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(77, 138);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 17);
+            this.resultLabel.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(841, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 26);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Fight";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultLabel2
+            // 
+            this.resultLabel2.AutoSize = true;
+            this.resultLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel2.Location = new System.Drawing.Point(453, 138);
+            this.resultLabel2.Name = "resultLabel2";
+            this.resultLabel2.Size = new System.Drawing.Size(0, 17);
+            this.resultLabel2.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 546);
+            this.Controls.Add(this.resultLabel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lambda8);
+            this.Controls.Add(this.lambda7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lambda6);
+            this.Controls.Add(this.lambda5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lambda4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lambda3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lambda2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lambda1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lambda1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lambda8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown lambda1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown lambda2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown lambda3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown lambda4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown lambda5;
+        private System.Windows.Forms.NumericUpDown lambda6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown lambda7;
+        private System.Windows.Forms.NumericUpDown lambda8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label winnerLabel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultLabel2;
+    }
+}
+
---
+++ Lab12/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12
+{
+    public partial class Form1 : Form
+    {
+        public Random rnd = new Random();
+        public const int TeamCount = 8;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Team> teams = new List<Team>();
+            teams = initTeam(teams);
+            Game(teams);
+        }
+
+        public void Game(List<Team> teams)
+        {
+            resultLabel.Text = "";
+            resultLabel2.Text = "";
+            int winScore = 0;
+            for (var i = 0; i < TeamCount - 1; i++)
+            {
+                if (i < 3)
+                {
+                    resultLabel.Text += "Team " + (i + 1);
+                    for (var j = i + 1; j < TeamCount; j++)
+                    {
+                        resultLabel.Text += " vs Team" + (j + 1) + ": " + match(teams[i], teams[j]) + "\n            ";
+                    }
+                    resultLabel.Text += "\n";
+                }
+                else
+                {
+                    resultLabel2.Text += "Team " + (i + 1);
+                    for (var j = i + 1; j < TeamCount; j++)
+                    {
+                        resultLabel2.Text += " vs Team" + (j + 1) + ": " + match(teams[i], teams[j]) + "\n            ";
+                    }
+                    resultLabel2.Text += "\n";
+                }
+                if (winScore < teams[i].goals)
+                {
+                    winScore = teams[i].goals;
+                }
+            }
+            showWinners(teams, winScore);
+        }
+
+        public void showWinners(List<Team> teams, int winScore)
+        {
+            winnerLabel.Text = "";
+            for (var i = 0; i < teams.Count; i++)
+            {
+                if (teams[i].goals == winScore)
+                {
+                    winnerLabel.Text += "Team " + (i + 1) + "\n";
+                }
+            }
+        }
+
+
+        public string match(Team t, Team s)
+        {
+            int goalsT, goalsS; double sum = 0;
+            for (goalsT = -1; sum >= -t.lambda; goalsT++)
+                sum += Math.Log(rnd.NextDouble());
+            t.goals += goalsT; sum = 0;
+            for (goalsS = -1; sum >= -s.lambda; goalsS++)
+            {
+                sum += Math.Log(rnd.NextDouble());
+            }
+            s.goals += goalsS;
+            var ans = goalsT.ToString() + " : " + goalsS.ToString();
+            return ans;
+        }
+
+
+        private List<Team> initTeam(List<Team> t)
+        {
+            t.Add(new Team() { lambda = (int)lambda1.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda2.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda3.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda4.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda5.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda6.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda7.Value, goals = 0 });
+            t.Add(new Team() { lambda = (int)lambda8.Value, goals = 0 });
+            return t;
+        }
+    }
+
+    public class Team
+    {
+        public int lambda, goals;
+    }
+
+}
